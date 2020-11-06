6c8227cea02c167d667b737be9abed689499214a
Shin, Ilsuk <actionshin@gmail.com>
2020-11-06 15:46:14 +0900

    Comment 테이블 추가

--- main/models.py
+++ main/models.py
@@ -6,3 +6,10 @@ class Post(db.Model):
     subject = db.Column(db.String(200), nullable=False)
     content = db.Column(db.Text(), nullable=False)
     create_date = db.Column(db.DateTime(), nullable=False)
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id', ondelete='CASCADE'))
+    post = db.relationship('Post', backref=db.backref('comment_set'))
+    content = db.Column(db.Text(), nullable=False)
+    create_date = db.Column(db.DateTime(), nullable=False)
